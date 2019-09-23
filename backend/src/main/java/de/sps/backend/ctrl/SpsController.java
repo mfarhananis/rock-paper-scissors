018d6b11224f62465eb8da826be1753f52ca1c25
Muhammad Farhan <mfarhan@live.de>
2019-09-23 08:37:42 +0200

    CORS Problem gehoben

--- backend/src/main/java/de/sps/backend/ctrl/SpsController.java
+++ backend/src/main/java/de/sps/backend/ctrl/SpsController.java
@@ -3,6 +3,7 @@ package de.sps.backend.ctrl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import de.sps.backend.model.Zeichen;
 import de.sps.backend.services.SpielService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4000")
 @RequestMapping("/sps")
 public class SpsController {
 	
